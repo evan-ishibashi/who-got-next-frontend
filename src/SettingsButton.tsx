4bcea8c1a07873f0b68de19bb23fdaeee825501e
Evan Ishibashi <evan.ishibashi@gmail.com>
2024-07-01 14:05:50 -0700

    updated to actually fix bug of form data loading on first render, check LS

--- src/SettingsButton.tsx
+++ src/SettingsButton.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { settingsContext } from "./QuickBasketballPage";
 import Popup from 'reactjs-popup';
 /** PointButton: Updates Scoreboard with given value
@@ -40,6 +40,13 @@ const SettingsButton = () => {
 
     }
 
+     // Handles When End game, or end break button is pressed
+     useEffect(()=>{
+        let quickBballSettings = localStorage.getItem('quickBballSettings');
+        if (quickBballSettings) setFormData(JSON.parse(quickBballSettings));
+        }
+    ,[])
+
     return (
         <div>
         <button type="button" className="button" onClick={() => setOpen(o => !o)}>
@@ -53,25 +60,25 @@ const SettingsButton = () => {
             <div className="modal">
 
                 <form onSubmit={handleSubmit}>
-                    <input id="gameClockMins" name="gameClockMins" className="w-10" onChange={handleChange} value={`${currSettings.gameClockMins}`} />
+                    <input id="gameClockMins" name="gameClockMins" className="w-10" onChange={handleChange} value={`${formData.gameClockMins}`} />
                     <label htmlFor="gameClockMins">{"Game Clock Minutes (0 - 99)"}</label>
                     <br />
-                    <input id="gameClockSecs" disabled name="gameClockSecs" className="w-10" onChange={handleChange} value={currSettings.gameClockSecs} />
+                    <input id="gameClockSecs" disabled name="gameClockSecs" className="w-10" onChange={handleChange} value={formData.gameClockSecs} />
                     <label htmlFor="gameClockSecs">{"Game Clock Seconds (0 - 59)"}</label>
                     <br />
-                    <input id="restClockMins" name="restClockMins" className="w-10" onChange={handleChange} value={`${currSettings.restClockMins}`} />
+                    <input id="restClockMins" name="restClockMins" className="w-10" onChange={handleChange} value={`${formData.restClockMins}`} />
                     <label htmlFor="restClockMins">{"Rest Clock Minutes (0 - 99)"}</label>
                     <br />
-                    <input id="restClockSecs" disabled name="restClockSecs" className="w-10" onChange={handleChange} value={currSettings.restClockSecs} />
+                    <input id="restClockSecs" disabled name="restClockSecs" className="w-10" onChange={handleChange} value={formData.restClockSecs} />
                     <label htmlFor="restSecs">{"Rest Clock Seconds (0 - 59)"}</label>
                     <br />
-                    <input id="winningScore" name="winningScore" className="w-10" onChange={handleChange} value={currSettings.winningScore} />
+                    <input id="winningScore" name="winningScore" className="w-10" onChange={handleChange} value={formData.winningScore} />
                     <label htmlFor="winningScore">{"Score Needed to Win (0-59)"}</label>
                     <br />
-                    <input id="teamSize" name="teamSize" className="w-10"  onChange={handleChange} value={`${currSettings.teamSize}`} />
+                    <input id="teamSize" name="teamSize" className="w-10"  onChange={handleChange} value={`${formData.teamSize}`} />
                     <label htmlFor="teamSize">{"Players Per Team (1-20)"}</label>
                     <br />
-                    <select id="teamRotation" name="teamRotation" value={currSettings.teamRotation} onChange={handleChange}>
+                    <select id="teamRotation" name="teamRotation" value={formData.teamRotation} onChange={handleChange}>
                         <option value="winnerStays">Winner Stays On</option>
                         <option value="playTwo">Play Two and Off</option>
                         <option value="bothOff">Both Teams Off</option>
